48571343d94b8b2613724d5a45c62ae5ac05596e
Tyler Treat <ttreat31@gmail.com>
2014-01-16 00:17:17 -0600

    Handle user disconnects more gracefully

--- server/server.go
+++ server/server.go
@@ -1,114 +1,133 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "strings"
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
 )
-
 
 const PORT string = "6666"
 
-
 type Client struct {
-    incoming chan string
-    outgoing chan string
-    reader   *bufio.Reader
-    writer   *bufio.Writer
+	server   *Server
+	incoming chan string
+	outgoing chan string
+	reader   *bufio.Reader
+	writer   *bufio.Writer
 }
 
 func (client *Client) Read() {
-    for {
-        line, _ := client.reader.ReadString('\n')
-        client.incoming <- line
-    }
+	for {
+		line, err := client.reader.ReadString('\n')
+
+		if err != nil {
+			client.Remove()
+			return
+		}
+
+		client.incoming <- line
+	}
 }
 
 func (client *Client) Write() {
-    for data := range client.outgoing {
-        client.writer.WriteString(data)
-        client.writer.Flush()
-    }
+	for data := range client.outgoing {
+		client.writer.WriteString(data)
+		client.writer.Flush()
+	}
 }
 
 func (client *Client) Listen() {
-    go client.Read()
-    go client.Write()
+	go client.Read()
+	go client.Write()
 }
 
-func NewClient(connection net.Conn) *Client {
-    writer := bufio.NewWriter(connection)
-    reader := bufio.NewReader(connection)
+func (client *Client) Remove() {
+	for i := range client.server.clients {
+		if client.server.clients[i] == client {
+			fmt.Println("Someone left the chat :(")
+			client.server.clients = append(client.server.clients[:i], client.server.clients[i+1:]...)
+			break
+		}
+	}
+}
 
-    client := &Client{
-        incoming: make(chan string),
-        outgoing: make(chan string),
-        reader: reader,
-        writer: writer,
-    }
+func NewClient(server *Server, connection net.Conn) *Client {
+	writer := bufio.NewWriter(connection)
+	reader := bufio.NewReader(connection)
 
-    client.Listen()
+	client := &Client{
+		server:   server,
+		incoming: make(chan string),
+		outgoing: make(chan string),
+		reader:   reader,
+		writer:   writer,
+	}
 
-    return client
+	client.Listen()
+
+	return client
 }
 
 type Server struct {
-    clients []*Client
-    joins chan net.Conn
-    incoming chan string
-    outgoing chan string
+	clients  []*Client
+	joins    chan net.Conn
+	incoming chan string
+	outgoing chan string
 }
 
 func (server *Server) Broadcast(data string) {
-    fmt.Println(strings.TrimRight(data, " \t\r\n"))
-    for _, client := range server.clients {
-        client.outgoing <- data
-    }
+	fmt.Println(strings.TrimRight(data, " \t\r\n"))
+	for _, client := range server.clients {
+		client.outgoing <- data
+	}
 }
 
 func (server *Server) Join(connection net.Conn) {
-    client := NewClient(connection)
-    server.clients = append(server.clients, client)
-    go func() { for { server.incoming <- <-client.incoming } }()
+	client := NewClient(server, connection)
+	server.clients = append(server.clients, client)
+	go func() {
+		for {
+			server.incoming <- <-client.incoming
+		}
+	}()
 }
 
 func (server *Server) Listen() {
-    go func() {
-        for {
-            select {
-            case data := <-server.incoming:
-                server.Broadcast(data)
-            case conn := <-server.joins:
-                server.Join(conn)
-            }
-        }
-    }()
+	go func() {
+		for {
+			select {
+			case data := <-server.incoming:
+				server.Broadcast(data)
+			case conn := <-server.joins:
+				server.Join(conn)
+			}
+		}
+	}()
 }
 
 func NewServer() *Server {
-    server := &Server{
-        clients: make([]*Client, 0),
-        joins: make(chan net.Conn),
-        incoming: make(chan string),
-        outgoing: make(chan string),
-    }
+	server := &Server{
+		clients:  make([]*Client, 0),
+		joins:    make(chan net.Conn),
+		incoming: make(chan string),
+		outgoing: make(chan string),
+	}
 
-    server.Listen()
+	server.Listen()
 
-    return server
+	return server
 }
 
 func main() {
-    server := NewServer()
+	server := NewServer()
 
-    listener, _ := net.Listen("tcp", ":" + PORT)
-    fmt.Println("Chat server started on port " + PORT)
+	listener, _ := net.Listen("tcp", ":"+PORT)
+	fmt.Println("Chat server started on port " + PORT)
 
-    for {
-        conn, _ := listener.Accept()
-        fmt.Println("User joined the chat!")
-        server.joins <- conn
-    }
+	for {
+		conn, _ := listener.Accept()
+		fmt.Println("Someone joined the chat :D")
+		server.joins <- conn
+	}
 }
-
