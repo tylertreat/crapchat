6a06d119013bbe07135812373e19b411d9a56fbb
Tyler Treat <ttreat31@gmail.com>
2014-01-16 00:18:40 -0600

    Format client code with gofmt

--- client/client.go
+++ client/client.go
@@ -1,93 +1,90 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "log"
-    "net"
-    "os"
-    "os/exec"
-    "runtime"
-    "strings"
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
 )
-
 
 const PORT string = "6666"
 
-
 type Client struct {
-    incomingReader *bufio.Reader
-    outgoingReader *bufio.Reader
-    writer         *bufio.Writer
+	incomingReader *bufio.Reader
+	outgoingReader *bufio.Reader
+	writer         *bufio.Writer
 }
 
 func outputMessage(message string) {
-    fmt.Println(message)
+	fmt.Println(message)
 
-    command := ""
-    if runtime.GOOS == "darwin" {
-        command = "say"
-    } else if runtime.GOOS == "linux" {
-        command = "espeak"
-    }
+	command := ""
+	if runtime.GOOS == "darwin" {
+		command = "say"
+	} else if runtime.GOOS == "linux" {
+		command = "espeak"
+	}
 
-    exec.Command(command, message).Run()
+	exec.Command(command, message).Run()
 }
 
 func (client *Client) ReadIncoming() {
-    for {
-        line, err := client.incomingReader.ReadString('\n')
+	for {
+		line, err := client.incomingReader.ReadString('\n')
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-        line = strings.TrimRight(line, " \t\r\n")
-        outputMessage(line)
-    }
+		line = strings.TrimRight(line, " \t\r\n")
+		outputMessage(line)
+	}
 }
 
 func (client *Client) ReadOutgoing() {
-    for {
-        line, _ := client.outgoingReader.ReadString('\n')
-        client.writer.WriteString(line)
-        client.writer.Flush()
-    }
+	for {
+		line, _ := client.outgoingReader.ReadString('\n')
+		client.writer.WriteString(line)
+		client.writer.Flush()
+	}
 }
 
 func (client *Client) Start() {
-    go client.ReadIncoming()
-    client.ReadOutgoing()
+	go client.ReadIncoming()
+	client.ReadOutgoing()
 }
 
 func NewClient(connection net.Conn) *Client {
-    incomingReader := bufio.NewReader(connection)
-    outgoingReader := bufio.NewReader(os.Stdin)
-    writer := bufio.NewWriter(connection)
+	incomingReader := bufio.NewReader(connection)
+	outgoingReader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(connection)
 
-    client := &Client{
-        incomingReader: incomingReader,
-        outgoingReader: outgoingReader,
-        writer: writer,
-    }
+	client := &Client{
+		incomingReader: incomingReader,
+		outgoingReader: outgoingReader,
+		writer:         writer,
+	}
 
-    return client
+	return client
 }
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: ", os.Args[0], "host")
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "host")
+		os.Exit(1)
+	}
 
-    host := os.Args[1]
-    conn, err := net.Dial("tcp", host + ":" + PORT)
+	host := os.Args[1]
+	conn, err := net.Dial("tcp", host+":"+PORT)
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    client := NewClient(conn)
-    client.Start()
+	client := NewClient(conn)
+	client.Start()
 }
-
